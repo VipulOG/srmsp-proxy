3be7ea93a2b477565d36a14c6409e3cb8268ff3e
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2024-01-20 21:47:41 +0530

    add expected attendance calendar

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import 'dotenv/config';
-import { StringToStream, streamToString } from "./utils.js";
+import "dotenv/config";
 
 const baseURL = process.env.host;
 const targetURL = "https://sp.srmist.edu.in";
@@ -10,16 +9,5 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 const axiosInstance = axios.create({
   baseURL: targetURL,
 });
-
-axiosInstance.interceptors.response.use(
-  async (response) => {
-    if (response.headers["content-type"].includes("text/html")) {
-      const html = await streamToString(response.data);
-      const newHtml = html.replace(new RegExp(targetURL, "g"), baseURL);
-      response.data = new StringToStream(newHtml);
-    }
-    return response;
-  },
-);
 
 export { axiosInstance };
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,11 @@
 import { axiosInstance } from "./axiosConfig.js";
+import { streamToString } from "./utils.js";
+import { parse } from "node-html-parser";
+
+const attendancePageURL =
+  "/srmiststudentportal/students/report/studentAttendanceDetails.jsp";
+const timetablePageURL =
+  "/srmiststudentportal/students/report/studentTimeTableDetails.jsp";
 
 export default function configureRoutes(app) {
   app.all("*", async (req, res) => {
@@ -26,10 +33,159 @@ export default function configureRoutes(app) {
       res.set(responseHeaders);
 
       res.status(axiosResponse.status);
-      axiosResponse.data.pipe(res);
+
+      if (axiosResponse.headers["content-type"].includes("text/html")) {
+        const html = await streamToString(axiosResponse.data);
+        const newHtml = html.replace(
+          new RegExp(axiosInstance.defaults.baseURL, "g"),
+          process.env.host
+        );
+
+        if (req.originalUrl === attendancePageURL) {
+          handleAttendancePage(req, res, newHtml);
+        } else if (req.originalUrl === timetablePageURL) {
+          handleTimetablePage(req, res, newHtml);
+        } else res.send(newHtml);
+      } else axiosResponse.data.pipe(res);
     } catch (error) {
       console.error("Proxy Request Error:", error.message, error.stack);
       res.status(500).send("Internal Server Error");
     }
   });
+}
+
+function handleTimetablePage(req, res, html) {
+  if (req.headers["accept"].includes("text/html")) {
+    res.send(html);
+    return;
+  }
+  if (req.headers["accept"].includes("application/json")) {
+    const root = parse(html);
+    const scheduleData = {};
+    const subjects = {};
+
+    const headings = root.querySelector("thead tr:nth-child(2)");
+    const headingCells = headings.querySelectorAll("th");
+    const timeSlots = [...headingCells].map((cell) => cell.text.trim());
+
+    const tables = root.querySelectorAll("tbody");
+    const timeTable = tables[0];
+    const subjectTable = tables[1];
+
+    const timeTableRows = timeTable.querySelectorAll("tr");
+    const subjectTableRows = subjectTable.querySelectorAll("tr");
+
+    subjectTableRows.forEach((row) => {
+      const cells = row.querySelectorAll("td");
+      const subjectCode = cells[0].text.trim();
+      const subjectName = cells[1].text.trim();
+      subjects[subjectCode] = subjectName;
+    });
+
+    timeTableRows.forEach((row, day) => {
+      scheduleData[day] = {};
+      const cells = row.querySelectorAll("td");
+
+      for (let i = 1; i < cells.length; i++) {
+        const subjectCode = cells[i].text.trim();
+        if (subjectCode === "-") continue;
+        const timeSlot = timeSlots[i - 1];
+        scheduleData[day][i] = {
+          subjectCode,
+          timeSlot,
+          subjectName: subjects[subjectCode],
+        };
+      }
+    });
+    const data = JSON.stringify(scheduleData, null, 2);
+    res.set("Content-Type", "application/json");
+    res.send(data);
+  }
+}
+
+function handleAttendancePage(req, res, html) {
+  const root = parse(html);
+
+  const tables = root.querySelectorAll("table");
+  const attendanceTable = tables[0];
+  const lastRow = attendanceTable.querySelectorAll("tr").pop();
+  const lastRowCells = lastRow.querySelectorAll("td");
+  const totalClasses = lastRowCells[1].text.trim();
+  const totalPresent = lastRowCells[2].text.trim();
+
+  const main = root.querySelector("div");
+  main.appendChild(
+    parse(`
+<link rel="stylesheet" href="https://vipul.is-a.dev/SimpleCalendar.js/src/SimpleCalendar.css">
+<style>
+  #calendarContainer {
+    margin: unset;
+    padding: unset;
+    max-width: unset;
+    min-height: 460px;
+  }
+</style>
+<div id="calendarContainer" class="card-body mt-4"></div>
+  `)
+  );
+  root.appendChild(
+    parse(`
+<script>
+const totalClasses = ${totalClasses};
+const totalPresent = ${totalPresent};
+
+function loadScript(url) {
+  return new Promise((resolve, reject) => {
+    $.getScript(url, resolve);
+  });
+}
+
+function postData(url, data, dataType = 'json') {
+  return new Promise((resolve, reject) => {
+    $.post({
+      url: url,
+      data: data,
+      dataType: dataType,
+    })
+      .done((response) => resolve(response))
+      .fail((error) => reject(error));
+  });
+}
+
+async function initCalendar() {
+  await loadScript("https://vipul.is-a.dev/SimpleCalendar.js/src/SimpleCalendar.js");
+  const timetableData = await postData("${timetablePageURL}");
+  
+  const events = [];
+  const today = new Date();
+  const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0, 23, 59, 59);
+
+  var expectedTotalClasses = totalClasses;
+  var expectedTotalPresent = totalPresent;
+
+  for (let date = new Date(today.valueOf()); date <= lastDayOfMonth; date.setDate(date.getDate() + 1)) {
+    const day = date.getDay() - 1;
+    const daySchedule = timetableData[day];
+    const expectedClasses = (daySchedule) ? Object.keys(daySchedule).length : 0;
+
+    expectedTotalClasses += expectedClasses;
+    expectedTotalPresent += expectedClasses;
+    const expectedAttendance = (expectedTotalPresent / expectedTotalClasses) * 100;
+
+    events.push(
+      {
+        date: new Date(date.valueOf()),
+        title: "Expected Attendance: " + expectedAttendance.toFixed(2) + "%",
+      }
+    );
+  }
+
+  calendar = new SimpleCalendar('#calendarContainer', events);
+}
+
+initCalendar();
+</script>
+    `)
+  );
+  res.send(root.toString());
 }
